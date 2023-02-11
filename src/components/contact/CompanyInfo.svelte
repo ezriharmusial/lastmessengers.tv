3c6803b1946dd584f9ca6eeaf3e3f4a0d52230ae
ezriharmusial <ezriharmusial@gmail.com>
2023-02-11 15:36:50 +0100

    Initial Commit

---
+++ src/components/contact/CompanyInfo.svelte
@@ -0,0 +1,49 @@
+<script>
+  let lang = 'en';
+  const locale = {}
+  locale['en'] = {
+      "telephone": "Telephone",
+      "bankaccount": "IBAN / Bank nr.",
+      "tax": "Chamber of Commerce nr. "
+  }
+  locale['nl'] = {
+    "telephone": "Telefoon",
+      "bankaccount": "IBAN Rekening",
+      "tax": "KVK Nummer"
+  }
+</script>
+
+<nav class="landscape:md:flex w-3/4 justify-center mx-auto text-2xl">
+  <div class="flex flex-col text-center p-4">
+    <div>
+      <p class="font-bold">{ locale[lang].telephone } (HOLLAND)
+      </p>
+      <p class="title text-xl ">+31(0)6 285 86 335
+      </p>
+    </div>
+  </div>
+  <div class="flex flex-col text-center p-4">
+    <div>
+      <p class="font-bold">{ locale[lang].telephone } (AFRICA)
+      </p>
+      <p class="title text-xl ">+234 91 692 03 399
+      </p>
+    </div>
+  </div>
+  <div class="flex flex-col text-center p-4">
+    <div>
+      <p class="font-bold">{ locale[lang].bankaccount }
+      </p>
+      <p class="title text-xl ">NL92INGB0006639081
+      </p>
+    </div>
+  </div>
+  <div class="flex flex-col text-center p-4">
+    <div>
+      <p class="font-bold">{ locale[lang].tax }
+      </p>
+      <p class="title text-xl ">55762441
+      </p>
+    </div>
+  </div>
+</nav>
---
+++ src/components/contact/Contacts.svelte
@@ -0,0 +1,210 @@
+<script>
+  export let sectionClass = "";
+</script>
+
+<section class={sectionClass}>
+  <h4 class="title">CEO: ArtKids Foundation</h4>
+  <div class="card is-profile has-background-dark">
+    <div class="card-image">
+      <figure>
+        <div class="dcll-bgwrap" style="background-color:#635c57;"></div>
+        <a href="/contact-us">
+          <img src="/images/alternative_energy/Ezehk-2.jpg"
+            alt="Organize to Control"
+            ratio="56%"/>
+        </a>
+      </figure>
+    </div>
+    <div class="card-profile-image">
+      <figure class="image is-128x128">
+        <img src="/images/alternative_energy/Ezehk-1-7090-150x150.jpg" alt="" />
+      </figure>
+    </div>
+    <div class="card-content content text-center has-text-light">
+      <p class="title is-size-5 has-text-secondary">Eng. Ezekwesiri E. Obioha</p>
+      <p class="subtitle is-size-6 has-text-light">CEO: ArtKids Foundation</p>
+      <p>
+        e: <a href="mailto:info@artkidsfoundation.org"
+          class="has-text-secondary"
+          title="Email Eng. Ezekwesiri E. Obioha">info@artkidsfoundation.org</a>
+        <br>
+        t: <a href="tel:+234 (0)81 601 451 77"
+          class="has-text-secondary"
+          title="Call Eng. Ezekwesiri E. Obioha ">+234 (0)81 601 451 77
+        </a>
+      </p>
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://www.facebook.com/profile.php?id=100015798102483">
+        <!-- <Icon client:only icon={faFacebook} size="1.5x" /> -->
+      </a>
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://www.linkedin.com/pub/engr-obioha-ezekwesiri-ict/2b/ab6/421">
+        <!-- <Icon client:only icon={faLinkedin} size="1.5x" /> -->
+      </a>
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://twitter.com/ObiohaEzekwesir">
+        <!-- <Icon client:only icon={faTwitter} size="1.5x" /> -->
+      </a>
+    </footer>
+  </div>
+</section>
+
+<section class={sectionClass}>
+  <h4 class="title">Managing Director: ArtKids Foundation Europe</h4>
+  <div class="card is-profile has-background-dark">
+    <div class="card-image">
+      <figure>
+        <div class="dcll-bgwrap" style="background-color:#812d06;"></div>
+        <img
+          src="/images/alternative_energy/Biblequotes-Facebook-Shared-Image-Rastered-9-343x180.jpg"
+          alt="No More BS"
+          ratio="56%"/>
+      </figure>
+    </div>
+    <div class="card-profile-image">
+      <figure class="image is-128x128">
+        <img src="/images/alternative_energy/bas_profielfoto-150x150.jpg"
+               alt="Profielfoto Bas Bijleveld"/>
+      </figure>
+    </div>
+    <div class="card-content content text-center has-text-light">
+      <p class="title is-size-5 has-text-secondary">Bastiaan M. Bijleveld</p>
+      <p class="subtitle is-size-6 has-text-light">Managing Director: ArtKids Foundation Europe</p>
+      <p>
+        e: <a href="mailto:europe@artkidsfoundation.org"
+          class="has-text-secondary"
+          title="Email Bas Bijleveld">europe@artkidsfoundation.org</a>
+        <br>
+        t: <a href="tel:+31(0)6 285 86 335"
+          class="has-text-secondary"
+          title="Call Bas Bijleveld">+31(0)6 285 86 335</a>
+      </p>
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://www.facebook.com/Basbijleveld">
+        <!-- <Icon client:only icon={faFacebook} size="1.5x" /> -->
+      </a>
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://nl.linkedin.com/in/basbijleveld">
+        <!-- <Icon client:only icon={faLinkedin} size="1.5x" /> -->
+      </a>
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://www.youtube.com/channel/UC6Kzb3a2xKltZCHdYtHsvcQ">
+        <!-- <Icon client:only icon={faYoutube} size="1.5x" /> -->
+      </a>
+    </footer>
+  </div>
+</section>
+
+<section class={sectionClass}>
+  <h4 class="title">Director of International Business</h4>
+  <div class="card is-profile has-background-dark has-text-light">
+    <div class="card-image">
+      <figure>
+        <div class="dcll-bgwrap" style="background-color:#779aa8;"></div>
+        <img src="/images/alternative_energy/Biblequotes-Facebook-Shared-Image-Rastered-343x180.jpg"
+               alt="Back to Nature"
+               ratio="56%"/>
+      </figure>
+    </div>
+    <div class="card-profile-image">
+      <figure class="image is-128x128">
+        <img src="/images/alternative_energy/Ezri_0.jpg"
+          alt="Profielfoto Ezri" />
+      </figure>
+    </div>
+    <div class="card-content content text-center">
+      <p class="title is-size-5 has-text-secondary">Ezri D. Harmusial</p>
+      <p class="subtitle has-text-light is-size-6">Director of International Business: ArtKids Foundation</p>
+      <p>
+        e: <a href="mailto:international@artkidsfoundation.org"
+          class="has-text-secondary"
+          title="Email Ezri Harmusial">international@artkidsfoundation.org</a>
+        <br>
+        t: <a
+          class="has-text-secondary"
+          href="tel:+31(0)6 285 86 335" title="Call Ezri Harmusial">+31(0)6 285 86 335</a>
+      </p>
+    </div>
+    <footer class="card-footer">
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://www.facebook.com/ezriharmusial">
+        <!-- <Fa icon={faFacebook} size="1.5x" /> -->
+      </a>
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://nl.linkedin.com/in/ezri-harmusial">
+        <!-- <Fa icon={faLinkedin} size="1.5x" /> -->
+      </a>
+      <a class="card-footer-item has-text-secondary" target="_blank" href="https://twitter.com/ezriharmusial">
+        <!-- <Fa icon={faTwitter} size="1.5x" /> -->
+      </a>
+    </footer>
+  </div>
+</section>
+
+<section class={sectionClass}>
+  <h4 class="title">Director: ArtKids Foundation Africa</h4>
+  <div class="card is-profile has-background-dark">
+    <div class="card-image">
+      <figure>
+        <div class="dcll-bgwrap" style="background-color:#635c57;"></div>
+        <a href="/contact-us">
+          <img src="/images/alternative_energy/Biblequotes-Facebook-Shared-Image-Rastered-2-343x180.jpg"
+            alt=""
+            ratio="56%"/>
+        </a>
+      </figure>
+    </div>
+    <div class="card-profile-image">
+      <figure class="image is-128x128">
+        <img src="/images/alternative_energy/chibundu_mmeri_victory.jpg" alt="" />
+      </figure>
+    </div>
+    <div class="card-content content text-center has-text-light">
+      <p class="title is-size-5 has-text-secondary"> Chibundu Mmeri Obioha</p>
+      <p class="subtitle is-size-6 has-text-light">Director: ArtKids Foundation Africa</p>
+      <p>
+        e: <a href="mailto:africa@artkidsfoundation.org"
+          class="has-text-secondary"
+          title="Email Chibundu Mmeri Obioha">africa@artkidsfoundation.org</a>
+        <br>
+        t: <a href="tel:+234 (0)81 484 874 27"
+          class="has-text-secondary"
+          title="Call Chibundu Mmeri Victory">+234 (0)81 484 874 27
+        </a>
+      </p>
+    </div>
+  </div>
+</section>
+
+<section class={sectionClass}>
+  <h4 class="title">Senior Electricity Consultant/advisor</h4>
+  <div class="card is-profile has-background-dark">
+    <div class="card-image">
+      <figure>
+        <div class="dcll-bgwrap" style="background-color:#635c57;"></div>
+        <a href="/contact-us">
+          <img
+            src="/images/alternative_energy/Biblequotes-Facebook-Shared-Image-Rastered-3-343x180.jpg"
+            alt=""
+            ratio="56%"/>
+        </a>
+      </figure>
+    </div>
+    <div class="card-profile-image">
+      <figure class="image is-128x128">
+        <img src="/images/alternative_energy/Mazi-Chidiebere-S-Oledibe.jpg" alt="" />
+      </figure>
+    </div>
+    <div class="card-content content text-center has-text-light">
+      <p class="title is-size-5 has-text-secondary"> Mazi Chidiebere S. Oledibe</p>
+      <p class="subtitle is-size-6 has-text-light">Senior Electricity Consultant/advisor</p>
+      <p class="content">
+        e: <a href="mailto:c.s.oledibe@artkidsfoundation.org"
+          class="has-text-secondary"
+          title="Email Mazi Chidiebere S. Oledibe">c.s.oledibe@artkidsfoundation.org
+        </a>
+        <br>
+        t: <a href="tel:+234 80 308 748 49"
+          class="has-text-secondary"
+          title="Call Mazi Chidiebere S. Oledibe">+234 80 308 748 49
+        </a>
+      </p>
+    </div>
+  </div>
+</section>
