315750694516b092a2241e206d3ba33c5724dd49
ezriharmusial <ezriharmusial@gmail.com>
2023-02-16 14:35:09 +0100

    LInkcast order, Card hover

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+	const linkcasts = await getCollection('linkcasts');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
+		items: linkcasts.map((linkcast) => ({
+			...linkcast.data,
+			link: `/newscasts/${linkcast.slug}/`,
 		})),
 	});
 }
